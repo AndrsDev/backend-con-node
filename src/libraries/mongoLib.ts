b9f42c5f9cf3436606e56149039deba84e819c32
Andres Sanabria <andrs.dev@gmail.com>
2021-03-28 16:31:25 -0600

    feat(core): testing husky

--- src/libraries/mongoLib.ts
+++ src/libraries/mongoLib.ts
@@ -2,6 +2,7 @@ import { Db, DeleteWriteOpResultObject, FilterQuery, InsertOneWriteOpResult, Mon
 import { config } from 'config';
 import { Movie } from 'models/movie';
 
+
 const USER = encodeURIComponent(config.DB_USER);
 const PASSWORD = encodeURIComponent(config.DB_PASSWORD);
 const DB_HOST = encodeURIComponent(config.DB_HOST);
