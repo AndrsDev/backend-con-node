47f577b8e535fc00c88f848d94f020032866f16c
Andres Sanabria <andrs.dev@gmail.com>
2021-04-06 12:38:57 -0600

    feat(core): created auth strategies

---
+++ src/utils/auth/strategies/jwt.ts
@@ -0,0 +1,31 @@
+import passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import boom from '@hapi/boom';
+import UsersService from 'services/usersService';
+import { config } from 'config';
+
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: config.AUTH_JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async function (payload, callback) {
+      const usersService = new UsersService();
+
+      try {
+        const user = await usersService.getUser(payload.email);
+
+        if (!user) {
+          return callback(boom.unauthorized(), null);
+        }
+
+        delete user.password;
+
+        callback(null, { ...user, scopes: payload.scopes });
+      } catch (error) {
+        callback(error);
+      }
+    }
+  )
+);
